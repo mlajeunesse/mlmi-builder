07b68a787907660dcdd438715b027b07d964446b
Mathieu Lajeunesse <contact@mathieulajeunesse.com>
2019-07-03 16:08:54 -0400

    v0.11.0 — Remove legacy column config system from new grid

--- admin/js/mlmi-builder-admin.js
+++ admin/js/mlmi-builder-admin.js
@@ -45,64 +45,84 @@ let mlmi_builder = {
 		
 		self.columns = function(row) {
 			let cols_config = $(row).find("div.acf-field.cols_config input[type=radio]:not(:disabled):checked").val();
-			switch (cols_config) {
-				case '6':
-				case '5': case '5-X':
-				case '4': case 'X-4': case '4-X': case 'X-4-X':
-				case '3': case 'X-3': case '3-X': case 'X-3-X': {
-					$(row).find(".mlmi-builder-column[data-name=col_1]").css("width", "100%");
-					$(row).find(".mlmi-builder-column-option[data-name=col_1_option]").css("width", "100%").show();
-					$(row).find(".mlmi-builder-column-option[data-name=col_1_order]").css("width", "100%").show();
-					break;
+			
+			/* Columns configuration */
+			if (typeof cols_config == 'undefined') {
+				let columns_number = $(row).find("div.acf-field.select-cols-number select").val();
+				if (columns_number == 3) {
+					$(row).find(".mlmi-builder-column[data-name=col_1]").css("width", "33.333%").removeClass('d-none');
+					$(row).find(".mlmi-builder-column[data-name=col_2]").css("width", "33.333%").removeClass('d-none');
+					$(row).find(".mlmi-builder-column[data-name=col_3]").removeClass('d-none');
+				} else if (columns_number == 2) {
+					$(row).find(".mlmi-builder-column[data-name=col_1]").css("width", "50%").removeClass('d-none');
+					$(row).find(".mlmi-builder-column[data-name=col_2]").css("width", "50%").removeClass('d-none');
+					$(row).find(".mlmi-builder-column[data-name=col_3]").removeClass('d-none');
+				} else if (columns_number == 1) {
+					$(row).find(".mlmi-builder-column[data-name=col_1]").css("width", "100%").removeClass('d-none');
+					$(row).find(".mlmi-builder-column[data-name=col_2]").removeClass('d-none');
+					$(row).find(".mlmi-builder-column[data-name=col_3]").removeClass('d-none');
 				}
-				case '3-3':
-				case '5-7':
-				case '7-5': {
-					$(row).find(".mlmi-builder-column[data-name=col_1]").css("width", "50%");
-					$(row).find(".mlmi-builder-column[data-name=col_2]").css("width", "50%");
-					$(row).find(".mlmi-builder-column-option[data-name=col_1_option]").css("width", "50%").show();
-					$(row).find(".mlmi-builder-column-option[data-name=col_2_option]").css("width", "50%").show();
-					$(row).find(".mlmi-builder-column-option[data-name=col_1_order]").css("width", "50%").show();
-					$(row).find(".mlmi-builder-column-option[data-name=col_2_order]").css("width", "50%").show();
-					break;
+			
+			/* Legacy ACF columns */
+			} else {
+				switch (cols_config) {
+					case '6':
+					case '5': case '5-X':
+					case '4': case 'X-4': case '4-X': case 'X-4-X':
+					case '3': case 'X-3': case '3-X': case 'X-3-X': {
+						$(row).find(".mlmi-builder-column[data-name=col_1]").css("width", "100%");
+						$(row).find(".mlmi-builder-column-option[data-name=col_1_option]").css("width", "100%").show();
+						$(row).find(".mlmi-builder-column-option[data-name=col_1_order]").css("width", "100%").show();
+						break;
+					}
+					case '3-3':
+					case '5-7':
+					case '7-5': {
+						$(row).find(".mlmi-builder-column[data-name=col_1]").css("width", "50%");
+						$(row).find(".mlmi-builder-column[data-name=col_2]").css("width", "50%");
+						$(row).find(".mlmi-builder-column-option[data-name=col_1_option]").css("width", "50%").show();
+						$(row).find(".mlmi-builder-column-option[data-name=col_2_option]").css("width", "50%").show();
+						$(row).find(".mlmi-builder-column-option[data-name=col_1_order]").css("width", "50%").show();
+						$(row).find(".mlmi-builder-column-option[data-name=col_2_order]").css("width", "50%").show();
+						break;
+					}
+					case '2-4': case '1-2':
+					case '1-5': {
+						$(row).find(".mlmi-builder-column[data-name=col_1]").css("width", "40%").addClass('-c0');
+						$(row).find(".mlmi-builder-column[data-name=col_2]").css("width", "60%").removeClass('-c0');
+						$(row).find(".mlmi-builder-column-option[data-name=col_1_option]").css("width", "40%").addClass('-c0').show();
+						$(row).find(".mlmi-builder-column-option[data-name=col_2_option]").css("width", "60%").removeClass('-c0').show();
+						$(row).find(".mlmi-builder-column-option[data-name=col_1_order]").css("width", "40%").addClass('-c0').show();
+						$(row).find(".mlmi-builder-column-option[data-name=col_2_order]").css("width", "60%").removeClass('-c0').show();
+						break;
+					}
+					case '5-1':
+					case '4-2':
+					case 'X-3-2': {
+						$(row).find(".mlmi-builder-column[data-name=col_1]").css("width", "60%");
+						$(row).find(".mlmi-builder-column[data-name=col_2]").css("width", "40%");
+						$(row).find(".mlmi-builder-column-option[data-name=col_1_option]").css("width", "60%").show();
+						$(row).find(".mlmi-builder-column-option[data-name=col_2_option]").css("width", "40%").show();
+						$(row).find(".mlmi-builder-column-option[data-name=col_1_order]").css("width", "60%").show();
+						$(row).find(".mlmi-builder-column-option[data-name=col_2_order]").css("width", "40%").show();
+						break;
+					}
+					case '2-2-2': {
+						$(row).find(".mlmi-builder-column").css("width", "33.333%");
+						$(row).find(".mlmi-builder-column-option").css("width", "33.333%").show();
+						break;
+					}
 				}
-				case '2-4': case '1-2':
-				case '1-5': {
-					$(row).find(".mlmi-builder-column[data-name=col_1]").css("width", "40%").addClass('-c0');
-					$(row).find(".mlmi-builder-column[data-name=col_2]").css("width", "60%").removeClass('-c0');
-					$(row).find(".mlmi-builder-column-option[data-name=col_1_option]").css("width", "40%").addClass('-c0').show();
-					$(row).find(".mlmi-builder-column-option[data-name=col_2_option]").css("width", "60%").removeClass('-c0').show();
-					$(row).find(".mlmi-builder-column-option[data-name=col_1_order]").css("width", "40%").addClass('-c0').show();
-					$(row).find(".mlmi-builder-column-option[data-name=col_2_order]").css("width", "60%").removeClass('-c0').show();
-					break;
-				}
-				case '5-1':
-				case '4-2':
-				case 'X-3-2': {
-					$(row).find(".mlmi-builder-column[data-name=col_1]").css("width", "60%");
-					$(row).find(".mlmi-builder-column[data-name=col_2]").css("width", "40%");
-					$(row).find(".mlmi-builder-column-option[data-name=col_1_option]").css("width", "60%").show();
-					$(row).find(".mlmi-builder-column-option[data-name=col_2_option]").css("width", "40%").show();
-					$(row).find(".mlmi-builder-column-option[data-name=col_1_order]").css("width", "60%").show();
-					$(row).find(".mlmi-builder-column-option[data-name=col_2_order]").css("width", "40%").show();
-					break;
-				}
-				case '2-2-2': {
-					$(row).find(".mlmi-builder-column").css("width", "33.333%");
-					$(row).find(".mlmi-builder-column-option").css("width", "33.333%").show();
-					break;
-				}
+				$(row).find(".mlmi-builder-column-option[data-name=col_1]").addClass('-c0');
+				$(row).find(".mlmi-builder-column-option[data-name=col_2]").removeClass('-c0');
+				$(row).find(".mlmi-builder-column-option[data-name=col_3]").removeClass('-c0');
+				$(row).find(".mlmi-builder-column-option[data-name=col_1_order]").addClass('-c0').css('min-height', '');
+				$(row).find(".mlmi-builder-column-option[data-name=col_2_order]").removeClass('-c0').css('min-height', '');
+				$(row).find(".mlmi-builder-column-option[data-name=col_3_order]").removeClass('-c0').css('min-height', '');
+				$(row).find(".mlmi-builder-column-option[data-name=col_1_option]").addClass('-c0').find('.acf-label').remove();
+				$(row).find(".mlmi-builder-column-option[data-name=col_2_option]").removeClass('-c0').find('.acf-label').remove();
+				$(row).find(".mlmi-builder-column-option[data-name=col_3_option]").removeClass('-c0').find('.acf-label').remove();
 			}
-			$(row).find(".mlmi-builder-column-option[data-name=col_1]").addClass('-c0');
-			$(row).find(".mlmi-builder-column-option[data-name=col_2]").removeClass('-c0');
-			$(row).find(".mlmi-builder-column-option[data-name=col_3]").removeClass('-c0');
-			$(row).find(".mlmi-builder-column-option[data-name=col_1_order]").addClass('-c0').css('min-height', '');
-			$(row).find(".mlmi-builder-column-option[data-name=col_2_order]").removeClass('-c0').css('min-height', '');
-			$(row).find(".mlmi-builder-column-option[data-name=col_3_order]").removeClass('-c0').css('min-height', '');
-			$(row).find(".mlmi-builder-column-option[data-name=col_1_option]").addClass('-c0').find('.acf-label').remove();
-			$(row).find(".mlmi-builder-column-option[data-name=col_2_option]").removeClass('-c0').find('.acf-label').remove();
-			$(row).find(".mlmi-builder-column-option[data-name=col_3_option]").removeClass('-c0').find('.acf-label').remove();
-		
 		};
 		
 		return function() {
