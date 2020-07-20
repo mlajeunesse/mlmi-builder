ad5080df41e5b774671fd86fdeeeaa507b57d748
Mathieu Lajeunesse <contact@mathieulajeunesse.com>
2020-07-20 15:52:24 -0400

    v0.13.17 — Improve background choices

--- mlmi-builder.php
+++ mlmi-builder.php
@@ -3,14 +3,14 @@
 * Plugin Name:       MLMI Builder
 * Plugin URI:        https://mathieulajeunesse.com
 * Description:       Outil de construction avancé et extensible de pages Wordpress.
-* Version:           0.13.16
+* Version:           0.13.17
 * Author:            Mathieu Lajeunesse médias interactifs
 * Author URI:        https://mathieulajeunesse.com
 * Text Domain:       mlmi-builder
 * Domain Path:       /languages
 */
 
-define('MLMI_BUILDER_VERSION', '0.13.16');
+define('MLMI_BUILDER_VERSION', '0.13.17');
 
 // If this file is called directly, abort.
 if (!defined('WPINC')) {
