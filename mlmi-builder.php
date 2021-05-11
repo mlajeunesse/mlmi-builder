4725d486ce2856b8d7fda43c078a23cc1bc6b5ef
Myriam <71526504+myriam-mlmi@users.noreply.github.com>
2021-05-11 14:05:55 -0400

    v0.15.6 - tinymce blockquote

--- mlmi-builder.php
+++ mlmi-builder.php
@@ -3,7 +3,7 @@
 * Plugin Name:       MLMI Builder
 * Plugin URI:        https://mathieulajeunesse.com
 * Description:       Outil de construction avancé et extensible de pages Wordpress.
-* Version:           0.15.5
+* Version:           0.15.6
 * Author:            Mathieu Lajeunesse médias interactifs
 * Author URI:        https://mathieulajeunesse.com
 * Text Domain:       mlmi-builder
